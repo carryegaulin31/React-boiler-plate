20b7e158834a8c6634ccb4f8c19d597c47d855f1
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-06-09 22:15:35 -0400

    initial commit after building environment

--- .history/server_20210609221038.js
+++ .history/server_20210609221038.js
@@ -13,4 +13,4 @@ app.use(express.static('public'))
 app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
 
 app.listen(1337, () => {
-  console.log('Listening on port 1337...')})
+  console.log('Listening on port 1337...')}
